0577920c7258f0078b1a94720fb560c1a2069b6e
soonitoon <soonitoon@gmail.com>
2021-01-02 04:29:48 +0900

    add weather API

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import Loading from "./Loading";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
 
+const API_KEY = "3d328986457d6b23d5ff91e9ddccad53";
+
 export default class extends React.Component {
   state = {
     isLoading: true,
   };
+  getWeather = async () => {};
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
